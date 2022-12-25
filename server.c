1233c494669ac7616bb78239f077f1171e6db225
stefanaciudin <stefanaciudin@gmail.com>
2022-12-26 00:23:03 +0200

    modified execvp in execl

--- server.c
+++ server.c
@@ -17,20 +17,15 @@ char cmd_answer[MAX_ANSWER];    // stores the answer
 int client;
 SSL *ssl;
 
-int size_cd;
-int pipe_number = -1; // stores the number of pipes
-int fd_input, fd_output;
-int has_input = 0, has_output = 0, append = 0, error_append = 0; // checks if the command has redirection
-
-int shell_exit(char **args) // returns 0 to terminate execution
+int shell_exit() // returns -1 to terminate execution
 {
     printf("Client disconnected\n");
     return -1;
 }
 
-int shell_help(char **args)
+int shell_help()
 {
-    strcpy(cmd_answer, "For help info on commands, use the man command \n"); // printf -- client
+    strcat(cmd_answer, "For help info on commands, use the man command \n"); // printf -- client
     return 1;
 }
 
@@ -62,9 +57,8 @@ int shell_cd(char **args) // implementation of cd
     return 1;
 }
 
-int shell_launch(char **args) // executes system commands
+int shell_launch(char *command) // executes system commands
 {
-
     int status;
     int pipe_output[2];
     if ((pipe(pipe_output)) == -1)
@@ -77,27 +71,11 @@ int shell_launch(char **args) // executes system commands
     if (pid == 0)
     {
         // child
-        if (has_output) // redirects output to given file
-        {
+        // redirects output to pipe and gives the command answer to the parent process
+        close(pipe_output[READ]);
+        dup2(pipe_output[WRITE], STDOUT_FILENO);
 
-            if (!error_append) // ">" or ">>"
-            {
-                dup2(fd_output, STDOUT_FILENO);
-                close(fd_output);
-            }
-            else
-            {
-                dup2(fd_output, STDERR_FILENO); // "2>" or "2>>"
-                close(fd_output);
-            }
-        }
-
-        if (!has_output) // redirects output to pipe and gives the command answer to the parent process
-        {
-            close(pipe_output[READ]);
-            dup2(pipe_output[WRITE], STDOUT_FILENO);
-        }
-        if (execvp(args[0], args) == -1)
+        if (execl("/bin/bash", "/bin/bash", "-c", command, (char *)NULL) == -1)
             handle_error("exec error");
         exit(10);
     }
@@ -134,25 +112,22 @@ int shell_execution(char **args) // executes built in commands or launches syste
     switch (val)
     {
     case 1:
-        return shell_help(args);
+        return shell_help();
     case 2:
         return shell_cd(args);
     case 3:
-        return shell_exit(args);
+        return shell_exit();
     default:
         break;
     }
-    return shell_launch(args);
+    return shell_launch(cmd_received);
 }
 
 char **shell_split_line(char *line)
 {
-    int bufsize = BUFF_SIZE;
     int poz = 0;
-    char **args = malloc(bufsize * sizeof(char *));
+    char **args = malloc(BUFF_SIZE * sizeof(char *));
     char *p;
-    char **args_copy;
-
     if (!args)
         handle_error("allocation in split_line");
     p = strtok(line, DELIM);
@@ -160,122 +135,29 @@ char **shell_split_line(char *line)
     {
         args[poz] = p;
         poz++;
-
-        if (poz >= bufsize)
-        {
-            bufsize += BUFF_SIZE;
-            args_copy = args;
-            args = realloc(args, bufsize * sizeof(char *));
-            if (!args)
-            {
-                free(args_copy);
-                handle_error("allocation in split_line");
-            }
-        }
-
         p = strtok(NULL, DELIM);
     }
     args[poz] = NULL;
     return args;
 }
-char *shell_redirect(char *line)
-{
-    char check[MAX_COMMAND];
-    char input[128], output[128];
 
-    for (int i = 0; i < strlen(line); i++)
-    {
-        strcpy(check, &line[i]);
-        if (check[0] == '<')
-        {
-            line[i] = '\0';
-            strcpy(input, &line[i + 1]); // input file
-            has_input++;
-        }
-        if (check[0] == '>')
-        {
-            if (check[1] == '>') //">>"
-            {
-                append++;
-                line[i] = '\0';
-                line[i + 1] = '\0';
-                strcpy(output, &line[i + 2]);
-                has_output++;
-            }
-            else //">"
-            {
-                line[i] = '\0';
-                strcpy(output, &line[i + 1]); // output file
-                has_output++;
-            }
-        }
-        if (check[0] == '2')
-        {
-            if (check[1] == '>' && check[2] != '>') //"2>"
-            {
-                error_append++;
-                line[i] = '\0';
-                line[i + 1] = '\0';
-                strcpy(output, &line[i + 2]);
-                has_output++;
-            }
-            else if (check[1] == '>' && check[2] == '>') //"2>>"
-            {
-                error_append++;
-                append++;
-                line[i] = '\0';
-                line[i + 1] = '\0';
-                line[i + 2] = '\0';
-                strcpy(output, &line[i + 3]);
-                has_output++;
-            }
-        }
-    }
-
-    if (has_input)
-    {
-        if ((fd_input = open(input, O_RDONLY)) < 0)
-            handle_error_ret("opening the input file");
-        dup2(fd_input, STDIN_FILENO);
-        close(fd_input);
-    }
-    if (has_output)
-    {
-        if (error_append && !append) //"2>"
-        {
-            if ((fd_output = creat(output, 0644)) < 0)
-                handle_error("creating the output file");
-        }
-        else if ((error_append && append) || append) //"2>>" or ">>"
-        {
-            if ((fd_output = open(output, O_RDWR | O_APPEND | O_CREAT)) < 0) // if the file exists, we append to it
-                handle_error("creating the output file");
-        }
-        else if ((fd_output = creat(output, 0644)) < 0) // read write permissions for user and group, read permission for everyone else; ">"
-            handle_error("creating the output file");
-    }
-    return line;
-}
-
-void shell_loop(char *command)
+void shell_loop()
 {
     char **args;
     int status;
-    command = shell_redirect(command);
-    args = shell_split_line(command);
-    status = shell_execution(args);
-
+    if (strstr(cmd_received, "cd") != NULL) //also login :)
+        {
+            args = shell_split_line(cmd_received);
+            status=shell_execution(args);
+        }
+    else
+        status = shell_launch(cmd_received);
     SSL_write(ssl, cmd_answer, sizeof(cmd_answer));
     bzero(cmd_answer, sizeof(cmd_answer));
     bzero(cmd_received, sizeof(cmd_received));
-    bzero(command, MAX_COMMAND);
-    has_output = 0, has_input = 0, append = 0, error_append = 0;
-    pipe_number = -1;
-    free(args);
 
     if (status >= 0) // the exit command returns a status<0, and then the execution stops
     {
-        bzero(cmd_received, sizeof(cmd_received));
         int val;
         val = SSL_read(ssl, cmd_received, MAX_COMMAND);
         cmd_received[val] = '\0';
@@ -309,7 +191,7 @@ int main()
     int server;
 
     SSL_library_init();
-    ctx = InitServerCTX();                             // initialize SSL 
+    ctx = InitServerCTX();                             // initialize SSL
     LoadCertificates(ctx, "mycert.pem", "mycert.pem"); // load certs - by default file names are both "mycert.pem"
     server = open_listener(PORT);
     struct sockaddr_in from;
@@ -325,7 +207,7 @@ int main()
         if (client < 0)
             handle_error("accept");
         pid_t pid;
-        ssl = SSL_new(ctx);      // get new SSL state with context 
+        ssl = SSL_new(ctx);      // get new SSL state with context
         SSL_set_fd(ssl, client); // might have to move this after fork, pls check
         if ((pid = fork()) == -1)
             handle_error("fork");
@@ -340,12 +222,12 @@ int main()
                 {
                     SSL_read(ssl, cmd_received, MAX_COMMAND);
                     cmd_received[sizeof(cmd_received)] = '\0';
-                    shell_loop(cmd_received);
+                    shell_loop();
                 }
             }
             int sd;
-            sd = SSL_get_fd(ssl); // get socket connection 
-            SSL_free(ssl);        // release SSL state 
+            sd = SSL_get_fd(ssl); // get socket connection
+            SSL_free(ssl);        // release SSL state
             close(sd);            // close connection
         }
         // else
